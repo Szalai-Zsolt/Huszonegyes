1c059f9db5c3ff970b85d3c9db4342681f3bf47c
Szalai <tlosz9@gmail.com>
2024-06-01 12:44:01 +0200

    project elkezdve, gitignore letrejott

---
+++ src/main/Huszonegyes.java
@@ -0,0 +1,20 @@
+
+package main;
+
+
+public class Huszonegyes {
+
+   
+    public static void main(String[] args) {
+        jatek();
+    }
+
+    private static void jatek() {
+       inicializalas();
+    }
+
+    private static void inicializalas() {
+        
+    }
+    
+}
